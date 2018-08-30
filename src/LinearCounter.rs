4509747dfea8d1ab98e40f2f67d0a5cbdb4ea413
Denis Bazhenov <bazhenov@farpost.com>
2018-08-31 00:40:29 +1000

    LinearCounter tests

--- src/LinearCounter.rs
+++ src/LinearCounter.rs
@@ -4,10 +4,12 @@ pub struct LinearCounter {
   buffer: Vec<u8>
 }
 
+const MAX_BUFFER_SIZE: usize = (u32::max_value() / 8) as usize;
+
 impl LinearCounter {
 
   pub fn new(size: usize) -> LinearCounter {
-    if size > 1000000 {
+    if size > MAX_BUFFER_SIZE {
       panic!("Too much of buffer")
     }
     let mut buffer = Vec::with_capacity(size);
@@ -18,12 +20,26 @@ impl LinearCounter {
   }
 
   pub fn offer(&mut self, s: &str) {
-    let hash = md5::compute(s);
-    self.buffer[0] |= 1;
+    let digest = md5::compute(s);
+    let bit_idx = self.buffer_idx(&digest);
+    let bit_offset = bit_idx & 0x07;
+    let byte_offset = bit_idx >> 3;
+    self.buffer[byte_offset] |= 1 << bit_offset;
   }
 
   pub fn estimate(&self) -> u32 {
     0
+  }
+
+  fn buffer_idx(&self, digest: &md5::Digest) -> usize {
+    let mut num = digest[0] as usize;
+    num <<= 8;
+    num |= digest[1] as usize;
+    num <<= 8;
+    num |= digest[2] as usize;
+    num <<= 8;
+    num |= digest[3] as usize;
+    return num % self.buffer.len();
   }
 
   fn population_count(&self) -> u32 {
@@ -31,7 +47,7 @@ impl LinearCounter {
     for i in 0..self.buffer.len() {
       let byte = self.buffer[i];
       for j in 0..8 {
-        r += (byte & (1 << j)) as u32;
+        r += ((byte >> j) & 1) as u32;
       }
     }
     r
@@ -45,9 +61,20 @@ mod tests {
 
   #[test]
   fn it_works() {
-    let mut lc = LinearCounter::new(10);
-    lc.offer("Hello");
-    assert_eq!(lc.population_count(), 1);
-    //assert_eq!(lc.estimate(), 1)
+    for i in 1..10 {
+      let mut lc = LinearCounter::new(100000);
+      for j in 0..i {
+        let mut s = "str".to_string();
+        s.push_str(&j.to_string());
+        lc.offer(&s);
+      }
+      assert_eq!(lc.population_count(), i);
+    }
+  }
+
+  #[test]
+  fn zero_lc() {
+    let mut lc = LinearCounter::new(1000);
+    assert_eq!(lc.population_count(), 0)
   }
 }
