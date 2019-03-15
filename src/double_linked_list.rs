109e36fa7cabb69b8bb69b2f38964fa3b09c7a53
Denis Bazhenov <bazhenov@farpost.com>
2019-03-15 15:53:32 +1000

    NodeLink struct added

--- src/double_linked_list.rs
+++ src/double_linked_list.rs
@@ -1,7 +1,10 @@
+use std::cell::{Ref, RefMut};
 use std::rc::{Rc, Weak};
 use std::cell::RefCell;
 
-type Link = Option<Rc<RefCell<Node>>>;
+struct NodeLink(Rc<RefCell<Node>>);
+
+type Link = Option<NodeLink>;
 
 pub struct DoublyLinkedList {
 
@@ -11,34 +14,64 @@ pub struct DoublyLinkedList {
 
 struct Node {
   data: String,
-  next: Option<Rc<RefCell<Node>>>,
+  next: Link,
   prev: Weak<RefCell<Node>>
+}
+
+impl NodeLink {
+
+  fn new(node: Node) -> NodeLink {
+    NodeLink(Rc::new(RefCell::new(node)))
+  }
+
+  fn borrow(&self) -> Ref<Node> {
+    self.0.borrow()
+  }
+
+  fn borrow_mut(&self) -> RefMut<Node> {
+    self.0.borrow_mut()
+  }
+
+  fn weak(&self) -> Weak<RefCell<Node>> {
+    Rc::downgrade(&self.0)
+  }
+
+  fn upgrade_prev(&self) -> Option<NodeLink> {
+    self.0.borrow().prev.upgrade().map(NodeLink)
+  }
+}
+
+impl Clone for NodeLink {
+
+  fn clone(&self) -> NodeLink {
+    NodeLink(Rc::clone(&self.0))
+  }
 }
 
 impl Node {
 
-  fn new_standalone(value: &str) -> Link {
+  fn new_standalone(value: &str) -> NodeLink {
     let node = Node { data: value.to_string(), next: None, prev: Weak::new() };
-    Some(Rc::new(RefCell::new(node)))
+    NodeLink::new(node)
   }
 
-  fn new_tail(value: &str, old_tail: &Link) -> Link {
+  fn new_tail(value: &str, old_tail: &Link) -> NodeLink {
     let node = Node { data: value.to_string(), next: None, prev: weak_link(old_tail) };
-    Some(Rc::new(RefCell::new(node)))
+    NodeLink::new(node)
   }
 
-  fn new_head(value: &str, old_head: &Link) -> Link {
+  fn new_head(value: &str, old_head: &Link) -> NodeLink {
     let node = Node { data: value.to_string(), next: clone_link(old_head), prev: Weak::new() };
-    Some(Rc::new(RefCell::new(node)))
+    NodeLink::new(node)
   }
 }
 
 fn clone_link(link: &Link) -> Link {
-  link.as_ref().map(Rc::clone)
+  link.as_ref().map(NodeLink::clone)
 }
 
 fn weak_link(link: &Link) -> Weak<RefCell<Node>> {
-  link.as_ref().map(Rc::downgrade).unwrap()
+  link.as_ref().map(NodeLink::weak).unwrap()
 }
 
 impl DoublyLinkedList {
@@ -48,33 +81,36 @@ impl DoublyLinkedList {
   }
 
   fn iter(&self) -> DoublyLinkedListIterator {
-    DoublyLinkedListIterator { item: self.head.as_ref().map(Rc::clone) }
+    DoublyLinkedListIterator { item: self.head.as_ref().map(NodeLink::clone) }
   }
 
-  fn push_back(&mut self, value: &str) -> Rc<RefCell<Node>> {
+  fn push_back(&mut self, value: &str) -> NodeLink {
     let new_tail = if let Some(ref old_tail) = self.tail {
       let new_tail = Node::new_tail(value, &self.tail);
-      old_tail.borrow_mut().next = clone_link(&new_tail);
+      old_tail.borrow_mut().next = Some(new_tail.clone());
       new_tail
     } else {
-      self.head = Node::new_standalone(value);
-      clone_link(&self.head)
+      let head = Node::new_standalone(value);
+      self.head = Some(head.clone());
+      head.clone()
     };
-    self.tail = clone_link(&new_tail);
-    new_tail.unwrap()
+    self.tail = Some(new_tail.clone());
+    new_tail
   }
 
-  fn push_front(&mut self, value: &str) -> Rc<RefCell<Node>> {
-    let new_head = if let Some(ref old_head) = self.head  {
+  fn push_front(&mut self, value: &str) -> NodeLink {
+    let new_head : NodeLink = if let Some(ref old_head) = self.head  {
       let new_head = Node::new_head(value, &self.head);
-      old_head.borrow_mut().prev = weak_link(&new_head);
+      old_head.borrow_mut().prev = new_head.weak();
       new_head
     } else {
-      self.tail = Node::new_standalone(value);
-      clone_link(&self.tail)
+      let head = Node::new_standalone(value);
+      // updating tail beacause it's the first element in the list
+      self.tail = Some(head.clone());
+      head
     };
-    self.head = clone_link(&new_head);
-    new_head.unwrap()
+    self.head = Some(new_head.clone());
+    new_head
   }
 
   fn pop_front(&mut self) -> Option<String> {
@@ -92,7 +128,7 @@ impl DoublyLinkedList {
 
   fn pop_back(&mut self) -> Option<String> {
     let (value, new_tail) = if let Some(ref old_tail) = self.tail {
-      (Some(old_tail.borrow().data.clone()), old_tail.borrow().prev.upgrade())
+      (Some(old_tail.borrow().data.clone()), old_tail.upgrade_prev())
     } else {
       (None, None)
     };
@@ -116,7 +152,7 @@ impl DoublyLinkedList {
 }
 
 struct DoublyLinkedListIterator {
-  item: Option<Rc<RefCell<Node>>>
+  item: Option<NodeLink>
 }
 
 impl Iterator for DoublyLinkedListIterator {
@@ -126,8 +162,8 @@ impl Iterator for DoublyLinkedListIterator {
   fn next(&mut self) -> Option<Self::Item> {
     let(ret, next) = match self.item {
       Some(ref i) => {
-        let r = i.borrow();
-        (Some(r.data.clone()), r.next.as_ref().map(Rc::clone))
+        let r: Ref<Node> = i.borrow();
+        (Some(r.data.clone()), r.next.as_ref().map(NodeLink::clone))
       },
       None => (None, None)
     };
@@ -238,7 +274,7 @@ mod tests {
     assert!(head.borrow().next.is_none());
     let tail = list.push_back("world");
 
-    assert!(Rc::ptr_eq(&tail, head.borrow().next.as_ref().unwrap()));
+    assert!(Rc::ptr_eq(&tail.0, &head.borrow().next.as_ref().unwrap().0));
   }
 
   #[test]
@@ -247,16 +283,16 @@ mod tests {
     let node = list.push_back("first");
 
     // We shold have 3 references at this point: head, tail and returned one (node)
-    assert_eq!(3, Rc::strong_count(&node));
+    assert_eq!(3, Rc::strong_count(&node.0));
 
     // We still should have 3 refs: first element, tail of the list and "node"-binding
     list.push_front("second");
-    assert_eq!(3, Rc::strong_count(&node));
+    assert_eq!(3, Rc::strong_count(&node.0));
 
     // At this point tail reference is moved to another element. So we're expecting
     // 2 refs here: first element referencing second and "node" binding itself.
     list.push_back("third");
-    assert_eq!(2, Rc::strong_count(&node));
+    assert_eq!(2, Rc::strong_count(&node.0));
   }
 
   fn assert_empty(list: &mut DoublyLinkedList) {
