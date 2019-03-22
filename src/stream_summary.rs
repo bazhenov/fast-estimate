e4d3380b829578336d19a17eee51823ce5130488
Denis Bazhenov <bazhenov@farpost.com>
2019-03-22 17:20:36 +1000

    remove old code

--- src/stream_summary.rs
+++ src/stream_summary.rs
@@ -1,35 +1,21 @@
 use std::rc::{Rc, Weak};
 use std::collections::{HashMap};
 use std::cell::RefCell;
+use double_linked_list::{DoublyLinkedList, NodeLink};
 
 /// Stream Summary structure
 #[allow(dead_code)]
 pub struct StreamSummary {
   monitored_items: HashMap<String, Rc<Item>>,
-  buckets: HashMap<u32, Bucket>
-}
-
-// Bucket is a set of elements sharing the same frequency of occurency in a stream
-#[allow(dead_code)]
-pub struct Bucket {
-  head: Weak<Item>
-}
-
-impl Bucket {
-
-  fn new() -> Bucket {
-    Bucket { head: Weak::new() }
-  }
+  buckets: HashMap<u32, DoublyLinkedList<String>>
 }
 
 #[allow(dead_code)]
 pub struct Item {
   data: String,
+  //bucket_node: NodeLink<String>,
   epsilon: u32,
-  count: u32,
-  next: Option<Rc<RefCell<Item>>>,
-  prev: Weak<Item>,
-  bucket: Weak<Bucket>
+  count: u32
 }
 
 impl Item {
@@ -38,23 +24,11 @@ impl Item {
     let item = Item {
       data: data.to_string(),
       epsilon: 0,
-      count: 0,
-      next: None,
-      prev: Weak::new(),
-      bucket: Weak::new()
+      count: 0
     };
-    //let r = Rc::new(RefCell::new(item));
-    //r.borrow_mut().next = Some(Rc::clone(&r));
     item
   }
 
-}
-
-#[allow(dead_code)]
-fn get_bucket(buckets: &mut HashMap<u32, Bucket>, order: u32) -> &mut Bucket {
-  buckets.entry(order).or_insert_with(|| {
-    Bucket::new()
-  })
 }
 
 impl StreamSummary {
@@ -71,48 +45,6 @@ impl StreamSummary {
   }
 
   pub fn offer(&mut self, _data: &str) -> u32 {
-
-    // let item = self.monitored_items.entry(data.to_string()).or_insert_with(|| {
-    //   Rc::new(Item { data: data.to_string(), epsilon: 0, count: 0,
-    //     next: Rc::new(Item::new("Hi")), prev: Weak::new(), bucket: Weak::new() })
-    // });
-    //
-    // let new_count = item.count + 1;
-    // let bucket = get_bucket(&mut self.buckets, new_count);
-    //
-    // let count;
-    // {
-    //   let mut bucket = match item.bucket.upgrade() {
-    //     Some(r) => r,
-    //     None => panic!("Empty bucket reference on Item found")
-    //   };
-    //   match bucket.head.upgrade() {
-    //     Some(bucket_head) => {
-    //       if Rc::ptr_eq(&bucket_head, item) {
-    //         match Rc::get_mut(&mut bucket) {
-    //           Some(b) => b.head = Weak::new(),
-    //           None => panic!("Bucket can't be borrowed mutably")
-    //         }
-    //       }
-    //     },
-    //     None => panic!("Empty item reference on Bucket found")
-    //   }
-    //   if Rc::ptr_eq(&bucket.head.upgrade().unwrap(), item) {
-    //     match item.bucket.upgrade().as_mut() {
-    //       Some(r) => Rc::get_mut(r).unwrap().head = Weak::new(),
-    //       None => {}
-    //     };
-    //   }
-    //
-    //   println!("RC: strong: {}, weak: {}", Rc::strong_count(item), Rc::weak_count(item));
-    //   let i = Rc::get_mut(item).unwrap();
-    //   i.count += 1;
-    //   count = i.count;
-    // }
-    //
-    // bucket.head = Rc::downgrade(&Rc::clone(&item));
-    // Rc::get_mut(item).unwrap().bucket = Weak::new();
-    // count
     0
   }
 }
@@ -134,17 +66,5 @@ mod tests {
     // for i in 0..10 {
     //   assert_eq!(i + 1, s.offer("Hello"));
     // }
-  }
-
-  #[test]
-  fn items_are_organized_in_ring() {
-    let a: Item = Item::new("foo");
-    let b: Item = Item::new("bar");
-  }
-
-  #[test]
-  fn item_can_advance_on_item() {
-    let _item = Item::new("first");
-
   }
 }
