3c62f37981a846603ff2897bfcb318e3d77e5186
Denis Bazhenov <bazhenov@farpost.com>
2019-03-29 15:24:36 +1000

    First working version

--- src/main.rs
+++ src/main.rs
@@ -10,79 +10,20 @@ use std::io;
 use stream_summary::StreamSummary;
 use linear_counter::LinearCounter;
 
-fn main() {
-
-  let matches = build_cli().get_matches();
-
-  let stdin = io::stdin();
-  let mut stdout = io::stdout();
-  let mut line = String::new();
-
-  if let Some(matches) = matches.subcommand_matches("uniq") {
-    let size = value_t!(matches, "size", usize).unwrap_or_else(|e| e.exit());
-    let mut lc = LinearCounter::new(size);
-
-    loop {
-      line.clear();
-      match stdin.read_line(&mut line) {
-        Ok(n) => {
-          if n <= 0 {
-            break;
-          }
-          lc.offer(&line)
-        }
-        Err(e) => {
-          panic!("{:?}", e)
-        }
-      }
-    }
-
-    println!("{:}", lc.estimate())
-
-  } else if let Some(matches) = matches.subcommand_matches("top") {
-    let size = value_t!(matches, "size", usize).unwrap_or_else(|e| e.exit());
-    let mut summary = StreamSummary::with_capacity(size);
-
-    loop {
-      line.clear();
-      match stdin.read_line(&mut line) {
-        Ok(n) => {
-          if n <= 0 {
-            break;
-          }
-          summary.offer(&line);
-
-        }
-        Err(e) => {
-          panic!("{:?}", e)
-        }
-      }
-    }
-
-    for item in summary.estimate_top() {
-      println!("{} - {}", item.count, item.data)
-    }
-
-
-  } else {
-    build_cli().write_help(&mut stdout);
-  }
-
-
-}
+use std::process::exit;
 
 fn build_cli() -> App<'static, 'static> {
   let top = SubCommand::with_name("top")
-    .about("estimates a top-k values in a stream")
+    .about("Estimates a top-k values in a stream")
     .arg(Arg::with_name("size")
       .long("size")
       .short("s")
       .takes_value(true)
       .default_value("1000")
-      .help("capacity of a stream-summary"));
+      .help("Capacity of a stream-summary"));
 
   let uniq = SubCommand::with_name("uniq")
-    .about("estimate number of unique values in a stream")
+    .about("Estimate number of unique values in a stream")
     .arg(Arg::with_name("size")
       .short("s")
       .long("size")
@@ -90,5 +31,73 @@ fn build_cli() -> App<'static, 'static> {
       .default_value("100000")
       .takes_value(true));
 
-  App::new("Fast estimate").subcommands(vec![top, uniq])
+  App::new("Fast estimate")
+    .arg(Arg::with_name("help")
+      .long("help")
+      .short("h")
+      .help("Show this help"))
+    .subcommands(vec![top, uniq])
+}
+
+fn usage() {
+  build_cli().write_long_help(&mut io::stdout()).expect("Failed to write help");
+  exit(0);
+}
+
+fn main() {
+  let matches = build_cli().get_matches();
+
+  if matches.is_present("help") {
+    usage();
+  }
+
+  if let Some(matches) = matches.subcommand_matches("uniq") {
+    let size = value_t!(matches, "size", usize).unwrap_or_else(|e| e.exit());
+    let mut lc = LinearCounter::new(size);
+
+    stdin_line_loop(|line| lc.offer(line));
+
+    println!("{:}", lc.estimate())
+
+  } else if let Some(matches) = matches.subcommand_matches("top") {
+    let size = value_t!(matches, "size", usize).unwrap_or_else(|e| e.exit());
+    let mut summary = StreamSummary::with_capacity(size);
+
+    stdin_line_loop(|line| {summary.offer(line);});
+
+    for item in summary.estimate_top() {
+      println!("{:6} : {}", item.count, item.data)
+    }
+
+  } else {
+    usage();
+  }
+}
+
+fn trim_newline(s: &mut String) {
+  if s.ends_with('\n') {
+    s.pop();
+    if s.ends_with('\r') {
+      s.pop();
+    }
+  }
+}
+
+fn stdin_line_loop<F>(mut line_callback: F)
+  where F: FnMut(&str) {
+
+  let stdin = io::stdin();
+  let mut line = String::new();
+
+  loop {
+    line.clear();
+    match stdin.read_line(&mut line) {
+      Ok(n) if n <= 0 => break,
+      Ok(_) => {
+        trim_newline(&mut line);
+        line_callback(&line)
+      },
+      Err(e) => panic!("{:?}", e)
+    }
+  }
 }
